5a649c1c3ad9e19bff15c9108a2e0b3accf05edc
Ruben Gabrielian <gabrielianruben@gmail.com>
2021-02-22 14:08:03 +0400

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+const api = {
+  key: '90584c6a3c4ed8e8c626b0b2d67dbf62',
+  base: 'api.openweathermap.org/data/2.5/'
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app ">
+      <main>
+        <div className='search-box'>
+          <input 
+            type="text"
+            className='search-bar'
+            placeholder='Search...'
+          />
+        </div>
+      </main>
     </div>
   );
 }
